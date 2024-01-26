8908a51cb3898d8b3c3b172505ec71b35faa0f58
DoğukanKorkmaz <dkorkmaz2000@hotmail.com>
2024-01-26 22:17:56 +0300

    TODO 7 finished

--- main.py
+++ main.py
@@ -38,21 +38,21 @@ is_on = True
 def print_resources_report():
     for item in resources:
         print(f"{item.capitalize()}: {resources[item]}")
-    print(f"Money:${profit}")
+    print(f"Money:${profit}.")
 
 
 def compare(coffee_ingredients):
     not_enough = False
     for item in coffee_ingredients:
         if coffee_ingredients[item] <= resources[item]:
-            print(f"{item.capitalize()}, yeterli.")
+            print(f"{item.capitalize()}, enough.")
         else:
-            print(f"{item.capitalize()}, yetersiz.")
+            print(f"{item.capitalize()}, not enough.")
             not_enough = True
     if not_enough:
-        return False
-    else:
         return True
+    else:
+        return False
 
 
 def prosses_coins():
@@ -70,32 +70,46 @@ def is_transaction_successful(money_received, drink_cost):
     if money_received >= drink_cost:
         global profit
         change = round(money_received - drink_cost, 2)
-        print(f"Here is ${change} in change")
+        print(f"Here is ${change} in change.")
         profit += drink_cost
-        print("Transaction is successful")
         return True
     else:
-        print("Sorry that's not enough money. Money refunded")
         return False
 
 
-while is_on:
-    choise = input("What would you like? (espresso/latte/cappuccino)\n")
-    if choise == "off":
-        print("See you later!")
-        is_on = False
-        break
-    elif choise == "report":
-        print_resources_report()
-    elif choise == "espresso" or "latte" or "cappuccino":
-        drink = MENU[choise]
-        compare(drink["ingredients"])
+def update_resources(coffee_name, coffee_resourses_cost):
+    for item in coffee_resourses_cost:
+        resources[item] -= coffee_resourses_cost[item]
+    print(f"Here is your {coffee_name}☕")
 
-    total_cost = prosses_coins()
-    print(f"Total is : ${total_cost}")
-    coffee_cost = MENU[choise]["cost"]
 
-    if is_transaction_successful(total_cost, coffee_cost):
-        print("")
-    else:
-        print("brr")
+def main_program():
+    global is_on
+    while is_on:
+        choise = input("What would you like ? (espresso/latte/cappuccino)\n")
+        if choise == "off":
+            print("See you later!")
+            is_on = False
+        elif choise == "report":
+            print_resources_report()
+        elif choise == "espresso" or "latte" or "cappuccino":
+            drink = MENU[choise]
+            if compare(drink["ingredients"]):
+                print("Sorry these ingredients need to refresh. ☹")
+                is_on = False
+                break
+
+            total_cost = prosses_coins()
+            print(f"Total is : ${total_cost}")
+            coffee_cost = MENU[choise]["cost"]
+
+            if is_transaction_successful(total_cost, coffee_cost):
+                print("Transaction is successful.")
+                coffee = MENU[choise]["ingredients"]
+                update_resources(choise, coffee)
+            else:
+                print("Sorry that's not enough money. Money refunded.")
+
+
+if is_on:
+    main_program()
