6857c356cd37db825375c59c3b42b4e6bd2f5a00
DoğukanKorkmaz <dkorkmaz2000@hotmail.com>
2024-01-26 16:33:54 +0300

    Gather resources file and main rewrite the code

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
-from coffee_data import MENU, resources
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
 
-water = resources["water"]
-milk = resources["milk"]
-coffee = resources["coffee"]
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
 
-def print_resources(water,milk,coffee):
-    return f"Water:{water}\nMilk:{milk}\nCoffee:{coffee}"
+is_on = True
 
-while(1):
-    answer = input("What would you like? (espresso/latte/cappuccino): ").lower()
-
-    if answer == "off":
+while is_on:
+    choise = input("What would you like? (espresso/latte/cappuccino" )
+    if choise == "off":
+        print("See you later!")
+        is_on = False
         break
-    elif answer == "report":
-        print(print_resources(water, milk, coffee))
-    # elif answer == "espresso":
-    #
-    # elif answer == "latte":
-    #
-    # elif answer == "cappuccino":
-print("hi")
-
-
